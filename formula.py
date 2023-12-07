ce3464bfb21bd683d17453c4a04c309654e51b3d
jacob <jacobjeong7@gmail.com>
2023-12-07 01:43:47 -0500

    added data

---
+++ formula.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 14:12:37 2022
+
+Author: 417-DevOps
+Desc: Riot API Bootcamp Module 1 example
+"""
+
+import pandas as pd
+import json
+
+
+with open('1641207119.json') as f:
+    d = json.load(f)
+    print(d)
